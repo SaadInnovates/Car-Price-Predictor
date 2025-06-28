1174d13e83363b9c536f1151756fdf81822f89a6
Muhammad Saad Zubair <muhammadsaadzubair186@gmail.com>
2025-06-28 15:06:09 +0500

    Delete patch_sklearn.py

--- patch_sklearn.py
+++
@@ -1,6 +0,0 @@
-# patch_sklearn.py
-import sklearn.compose._column_transformer
-
-# Apply permanent fix for _RemainderColsList
-if not hasattr(sklearn.compose._column_transformer, '_RemainderColsList'):
-    setattr(sklearn.compose._column_transformer, '_RemainderColsList', list)
